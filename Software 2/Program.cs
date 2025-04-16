b756d07e400f142803a0b76dc695e39ee6ed3bc4
Ja1deR <jaiderromero8@gmail.com>
2025-04-16 11:49:53 -0500

    swagger

--- Software 2/Program.cs
+++ Software 2/Program.cs
@@ -4,19 +4,26 @@ using Software_2.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings:Cadena")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings:cadena")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -26,6 +33,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+
 
 app.Run();
